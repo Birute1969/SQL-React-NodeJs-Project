5af14b88244d46097ca7202fb1191b54c2c40d47
Birute1969 <107874553+Birute1969@users.noreply.github.com>
2023-01-28 19:59:31 +0200

    Structure of Pages + Readme file

--- app/app.js
+++ app/app.js
@@ -1,10 +1,24 @@
 const cors = require('cors');
 const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json);
+
+const mysqlConfig = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
+};
+
+const connection = mysql.createConnection(mysqlConfig);
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
